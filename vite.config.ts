957069db7d0ec1a89eecf712110707ccf10fd66b
SOMESH <somesh@SOMESHs-MacBook-Air.local>
2025-07-26 11:16:23 +0530

    images issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // This ensures assets are loaded relative to index.html
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    assetsInclude: ['**/*.jpg', '**/*.png', '**/*.jpeg', '**/*.gif'],
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
